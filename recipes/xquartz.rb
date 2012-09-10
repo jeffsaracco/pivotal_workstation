acef0e2bbb3d30ece375b9f4e921b55d88dfb588
Brian Cunnie <pair+cunnie@pivotallabs.com>
2012-09-10 10:21:11 -0700

    xquartz 2.7.3

--- recipes/xquartz.rb
+++ recipes/xquartz.rb
@@ -1,8 +1,8 @@
 pivotal_workstation_package "XQuartz" do
-  source "http://xquartz.macosforge.org/downloads/SL/XQuartz-2.7.2.dmg"
+  source "http://xquartz.macosforge.org/downloads/SL/XQuartz-2.7.3.dmg"
   action :install
-  volumes_dir "XQuartz-2.7.2"
-  #checksum "309c11099a8712db2f6f4fb3188e1c30d9f966f3e58a137b6c649a1dba448fce"
+  volumes_dir "XQuartz-2.7.3"
+  checksum "3e8d0b4ff0bb5325d653f3d2a58a11a212fa63a893a0b2d8e7c7edcc8d9bb4bd"
   type "pkg"
   package_id "org.macosforge.xquartz.pkg"
 end
